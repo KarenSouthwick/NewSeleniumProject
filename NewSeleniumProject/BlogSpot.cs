7ace800c8635d62091dbcd167928725cf740672f
KarenSouthwick <Karen.Southwick@Authenticateis.com>
2018-06-25 15:04:22 +0100

    these tests have been copied from the Mar2018 project

---
+++ NewSeleniumProject/BlogSpot.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using excel = Microsoft.Office.Interop.Excel;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+
+namespace NewSeleniumProject
+{
+    class BlogSpot
+    {
+        private string email;
+        private string pass;
+
+        [Test]
+        public void Test()
+        {
+            try
+            {
+                IWebDriver driver = new ChromeDriver();
+                driver.Navigate().GoToUrl("https://uat-platform.authenticateis.com/Account/Logon");
+                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+
+                excel.Application x1app = new excel.Application();
+                excel.Workbook x1workbook = x1app.Workbooks.Open(@"C:\temp\vince.xlsx");
+                excel._Worksheet x1worksheet = x1workbook.Sheets[1];
+                excel.Range x1range = x1worksheet.UsedRange;
+
+                int count = x1range.Rows.Count;
+                for (int i = 1; i <= count; i++)
+
+                {
+                    string email = (x1range.Cells[i, 1] as excel.Range).Value;
+                    string pass = (x1range.Cells[i, 2] as excel.Range).Value;
+
+                    driver.FindElement(By.Id("UserName")).SendKeys(email);
+                    driver.FindElement(By.Id("Password")).SendKeys(pass);
+                    driver.FindElement(By.Id("do-submit")).Click();
+                    driver.FindElement(By.CssSelector(".lock")).Click();
+                }
+
+                driver.Quit();
+                x1app.Quit();
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+        }
+
+    }
+}
---
+++ NewSeleniumProject/RandomProd.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace NewSeleniumProject
+{
+    class RandomProd
+    {
+        public static int GetRandomNumber(int min, int max)
+        {
+            Random random = new Random();
+            return random.Next(min, max);
+        }
+
+        [Test]
+        public void Test()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://uat-platform.authenticateis.com/Account/Logon");
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+
+            driver.FindElement(By.Id("UserName")).SendKeys("angelgaltrey");
+            driver.FindElement(By.Id("Password")).SendKeys("Aramark22");
+            driver.FindElement(By.Id("do-submit")).Click();
+
+            driver.FindElement(By.LinkText("Products")).Click();
+            driver.FindElement(By.LinkText("Product Catalogue")).Click();
+            driver.FindElement(By.PartialLinkText("Benson")).Click();
+
+            driver.FindElement(By.LinkText("create new")).Click();
+            driver.FindElement(By.Id("ProductLine_Name")).SendKeys("" + GetRandomNumber(100, 1000));
+            driver.FindElement(By.Id("ProductLine_ReferenceCode")).SendKeys("" + GetRandomNumber(16, 99));
+
+            driver.FindElement(By.XPath("(//input[@type='text'])[5]")).SendKeys("Cranswick Delico" + Keys.Enter);
+            driver.FindElement(By.CssSelector("[class='button action']")).Click();
+
+            driver.Quit();
+
+        }
+    }
+}
